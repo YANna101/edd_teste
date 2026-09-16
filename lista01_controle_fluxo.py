7ed148f68eef244cf62313714cdbe362106c1fa9
YANna101 <yanna.silva@escolar.ifrn.edu.br>
2026-09-16 17:26:56 +0000

    correção do professor Higor

--- lista01_controle_fluxo.py
+++ lista01_controle_fluxo.py
@@ -7,27 +7,38 @@ def imprimir_menor(numero1,numero2):
     else:
         print(f"{numero1} e ingual ao numero {numero2}")
 
-
+print("Questão 01")
 imprimir_menor(3,5)
 imprimir_menor(3,2)
 imprimir_menor(5,5)
 
+print("\n\n")
+
+
+print("Questão 02")
 #Escreva uma função que recebe um número n como parâmetro e imprime se n é positivo ou negativo
-def imprimir_sinal(simal1,sinal2):
-    if simal1>=0:
-        print(f"o sinal{simal1}é positivo")
+def imprimir_sinal(n):
     
-    elif simal1<0:
-        print(f"o sinal{simal1}é negativo")
-        if sinal2>=0:
-            print(f"o sinal{sinal2}é positivo")
-        elif sinal2<0:
-            print(f"o sinal{sinal2}é negativo")  
+    if n>=0:
+        print(f"O número {n} é positivo.")
+    else:
+        print(f"O número {n} é negativo.")
+    
+    # if simal1>=0:
+    #     print(f"o sinal{simal1}é positivo")
+    
+    # elif simal1<0:
+    #     print(f"o sinal{simal1}é negativo")
+    #     if sinal2>=0:
+    #         print(f"o sinal{sinal2}é positivo")
+    #     elif sinal2<0:
+    #         print(f"o sinal{sinal2}é negativo")  
 
-
+print("\n\n")
         
 
-  #Escreva uma função para imprimir o valor absoluto de um número.
+print("Questão 03")
+#Escreva uma função para imprimir o valor absoluto de um número.
 def imprimir_valor_absoluto(numero):
     if numero<0:
         print(f"o valor absoluto de {numero} é {numero*-1}")
@@ -35,11 +46,13 @@ def imprimir_valor_absoluto(numero):
         print(f"o valor absoluto de {numero} é {numero}")
 
 
-        imprimir_valor_absoluto(-5)
-        imprimir_valor_absoluto(5)
+imprimir_valor_absoluto(-5)
+imprimir_valor_absoluto(5)
+
+print("\n\n")
 
 
-
+print("Questão 04")
 #Escreva uma função que recebe dois números a e b como parâmetro e retorna True caso a soma dos dois seja maior que um terceiro parâmetro, chamado limite.
 def soma_maior_que_limite(a,b,limite):
     if a+b>limite:
@@ -47,11 +60,14 @@ def soma_maior_que_limite(a,b,limite):
     else:
         return False
     
-print(soma_maior_que_limite(10,20,15))
+print("Verifica se 10+20>15: " + soma_maior_que_limite(10,20,15))
+print("Verifica se 10+22>15: " + soma_maior_que_limite(10,2,15))
+
+print("\n\n")
 
 
 
-
+print("Questão 05")
 #Escreva uma função que recebe dois números (a e b) como parâmetro e retorna a quantidade (0, 1 ou 2) deles que é maior que um terceiro parâmetro, chamado limite.
 def quantidade_maior_que_limite(a, b, limite):
     quantidade = 0
@@ -60,29 +76,41 @@ def quantidade_maior_que_limite(a, b, limite):
     if b > limite:
         quantidade += 1
     return quantidade
-quantidade_maior_que_limite(10, 20, 15)
+
+print("Valores (10, 20, 15). Maiores que o limite: "+quantidade_maior_que_limite(10, 20, 15))
+print("Valores (10, 2, 15). Maiores que o limite: "+quantidade_maior_que_limite(10, 2, 15))
+print("Valores (100, 20, 15). Maiores que o limite: "+quantidade_maior_que_limite(100, 20, 15))
+
+print("\n\n")
 
 
 
-        #Escreva uma função que recebe um número como parâmetro e para cada número menor que o parâmetro, a função imprime “Fizz” se o número for múltiplo de três, imprime “Buzz” se o número for múltiplo de cinco, e imprime “FizzBuzz” se o número for múltiplo de três e cinco. Caso o número não seja múltiplo nem de três nem de cinco, ele deve ser impresso. Note que, ao contrário das funções anteriores, sua função não deve retornar nada. Ela precisa simplesmente imprimir o que foi pedido.
+print("Questão 06")
+#Escreva uma função que recebe um número como parâmetro e para cada número menor que o parâmetro, a função imprime “Fizz” se o número for múltiplo de três, imprime “Buzz” se o número for múltiplo de cinco, e imprime “FizzBuzz” se o número for múltiplo de três e cinco. Caso o número não seja múltiplo nem de três nem de cinco, ele deve ser impresso. Note que, ao contrário das funções anteriores, sua função não deve retornar nada. Ela precisa simplesmente imprimir o que foi pedido.
 def fizz_buzz(n):
     for i in range(n):
-        if i % 3 == 0 and i % 5 == 0:
+        if i % 3 == 0 and i % 5 == 0: # múltiplos de 3 e 5
             print("FizzBuzz")
-        elif i % 3 == 0:
+        elif i % 3 == 0: # múltiplos de 3
             print("Fizz")
-        elif i % 5 == 0:
+        elif i % 5 == 0: # múltiplos de 5
             print("Buzz")
-        else:
+        else: # o próprio valor
             print(i)
-            fizz_buzz(16)
-            fizz_buzz(30)
+
+print("fizz_buzz(5)")
+fizz_buzz(5)
+print("fizz_buzz(16)")
+fizz_buzz(16)
+print("fizz_buzz(30)")
+fizz_buzz(30)
+
+print("\n\n")
 
 
 
-
-
-            #Escreva uma função que, dado um número nota representando a nota de um estudante, converte o valor de nota para um conceito (A, B, C, D, E e F).
+print("Questão 07")
+#Escreva uma função que, dado um número nota representando a nota de um estudante, converte o valor de nota para um conceito (A, B, C, D, E e F).
 def converter_nota_para_conceito(nota):
     if nota >= 90:
         return "A"
@@ -106,42 +134,51 @@ print(converter_nota_para_conceito(45))
 print(converter_nota_para_conceito(35))
 print(converter_nota_para_conceito(25))
 
+print("\n\n")
 
 
 
+print("Questão 08")
 #Escreva uma função que recebe como entrada um número inteiro positivo n e retorne a soma de todos os inteiros positivos menores ou iguais a n.
-
 def soma_inteiros_ate_n(n):
     soma = 0
     for i in range(1, n + 1):
         soma += i
     return soma
-print(soma_inteiros_ate_n(5))
-print(soma_inteiros_ate_n(10))
+
+print("Soma dos inteiros menores que 5: "+soma_inteiros_ate_n(5))
+print("Soma dos inteiros menores que 10: "+soma_inteiros_ate_n(10))
+
+
+print("\n\n")
 
 
 
+print("Questão 09")
 #Escreva uma função que recebe como entrada um número ano e retorna True caso ano seja bissexto. Caso contrário, retorne False.
-
 def eh_bissexto(ano):
     if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
         return True
     else:
         return False
 
-print(eh_bissexto(2020))
-print(eh_bissexto(2021))
+print("2020 é bissexto? "+eh_bissexto(2020))
+print("2021 é bissexto? "+eh_bissexto(2021))
 
 
 
+print("\n\n")
 
 
+
+print("Questão 10")
 #Escreva uma função que recebe como entrada um número n e imprime todas as potências de 2 menores ou iguais a n.
-
 def imprimir_potencias_de_2(n):
-    potencia = 1
+    
+    print(f"Potências de 2 menores que {n}: ")
+    potencia = 1 # representa 2^0 = 1
     while potencia <= n:
-        print(potencia)
+        print(potencia, end="; ")
         potencia *= 2
 
 imprimir_potencias_de_2(16)
@@ -149,9 +186,12 @@ imprimir_potencias_de_2(30)
 
 
 
+print("\n\n")
 
+
+
+print("Questão 11")
 #Escreva uma função que recebe como entrada um número inteiro positivo n e imprime a representação binária desse número.
-
 def imprimir_representacao_binaria(n):
     if n < 0:
         print("O número deve ser positivo.")
@@ -163,7 +203,9 @@ def imprimir_representacao_binaria(n):
         binario = str(n % 2) + binario
         n //= 2
     print(binario)
-    print("Representação binária de 10:")
+
+print("Representação binária de 10:")
 imprimir_representacao_binaria(10)
+
 print("Representação binária de 15:")
 imprimir_representacao_binaria(15)
